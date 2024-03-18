d8819d241813eacb164c201dc85d6ce00902839b
Patrice <kunhoulee@gmail.com>
2024-03-18 18:07:36 +0800

    feat: add error reason

---
+++ internal/common/reason/reason.go
@@ -0,0 +1,9 @@
+package reason
+
+const (
+	Success            = "success"
+	UnknownError       = "unknown_error"
+	RequestFormatError = "request_format_error"
+
+	TaskNotFound = "error.task.not_found"
+)
