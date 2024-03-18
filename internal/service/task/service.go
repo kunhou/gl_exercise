32669bca00b165c572899fbcacb26431ce86d43b
Patrice <kunhoulee@gmail.com>
2024-03-18 15:41:35 +0800

    chores: add comment on service layer function

--- internal/service/task/service.go
+++ internal/service/task/service.go
@@ -7,6 +7,7 @@ import (
 )
 
 //go:generate mockgen -source ./service.go -destination=../mocks/task.go -package=mocks
+// ITaskRepository task repository
 type ITaskRepository interface {
 	List(ctx context.Context) ([]entity.Task, error)
 	Create(ctx context.Context, task entity.Task) (entity.Task, error)
@@ -14,10 +15,12 @@ type ITaskRepository interface {
 	Delete(ctx context.Context, id int) error
 }
 
+// Service task service
 type Service struct {
 	taskRepo ITaskRepository
 }
 
+// New new task service
 func New(taskRepo ITaskRepository) *Service {
 	return &Service{
 		taskRepo: taskRepo,
--- internal/service/task/task.go
+++ internal/service/task/task.go
@@ -6,21 +6,25 @@ import (
 	"github/kunhou/gl_exercise/internal/entity"
 )
 
+// List list tasks
 func (s *Service) List(ctx context.Context) (tasks []entity.Task, err error) {
 	tasks, err = s.taskRepo.List(ctx)
 	return
 }
 
+// Create create task
 func (s *Service) Create(ctx context.Context, task entity.Task) (result entity.Task, err error) {
 	result, err = s.taskRepo.Create(ctx, task)
 	return
 }
 
+// Update update task
 func (s *Service) Update(ctx context.Context, id int, task entity.Task) (result entity.Task, err error) {
 	result, err = s.taskRepo.Update(ctx, id, task)
 	return
 }
 
+// Delete delete task
 func (s *Service) Delete(ctx context.Context, id int) error {
 	return s.taskRepo.Delete(ctx, id)
 }
